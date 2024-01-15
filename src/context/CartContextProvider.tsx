a94a4b7d24f566aec52be5e8a5512ab42cd16fe1
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2024-01-15 22:39:11 +0100

    debug consoele.logs

--- src/context/CartContextProvider.tsx
+++ src/context/CartContextProvider.tsx
@@ -10,10 +10,10 @@ export default function ContextProvider({ children }: PropsWithChildren) {
   const [cart, setCart] = useState<TCartItem[]>(storedCart)
 
   const addProduct = (product: TProduct, quantity: number = 1) => {
-    console.log('clicked')
+    console.log('clicked addProduct')
 
     setCart((prevCartItems) => {
-      console.log('prevCartItems: ', prevCartItems)
+      console.log('addProduct prevCartItems: ', prevCartItems)
       if (prevCartItems.some((crtItem) => crtItem.product.id === product.id)) {
         const updatedCart = prevCartItems.map((crtItem) =>
           crtItem.product.id === product.id
@@ -23,7 +23,7 @@ export default function ContextProvider({ children }: PropsWithChildren) {
               }
             : crtItem
         )
-        console.log('updatedCart', updatedCart)
+        console.log('addProduct updatedCart', updatedCart)
         setStoredCart(updatedCart)
         return updatedCart
       } else {
@@ -35,8 +35,12 @@ export default function ContextProvider({ children }: PropsWithChildren) {
     })
   }
 
-  const removeProduct = (product: TProduct) =>
+  const removeProduct = (product: TProduct) => {
+    console.log('clicked removeProduct')
+
     setCart((prevCartItems) => {
+      console.log('removeProduct prevCartItems: ', prevCartItems)
+
       const cartItem = prevCartItems.find((p) => p.product.id === product.id)!
       if (cartItem.quantity > 1) {
         const updatedCart = prevCartItems.map((crtItem) =>
@@ -44,7 +48,7 @@ export default function ContextProvider({ children }: PropsWithChildren) {
             ? { product: crtItem.product, quantity: crtItem.quantity-- }
             : crtItem
         )
-
+        console.log('removeProduct updatedCart: ', updatedCart)
         setStoredCart(updatedCart)
         return updatedCart
       } else {
@@ -56,6 +60,7 @@ export default function ContextProvider({ children }: PropsWithChildren) {
         return updatedCart
       }
     })
+  }
 
   const removeAll = () => {
     removeStoredCart()
